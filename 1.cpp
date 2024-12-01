4c0d11643624f0639dd839d944c3dbb4c9f1a8e7
Aleksey <morshnev.aleksey@gmail.com>
2024-12-01 10:05:28 +0100

    Day 1 solved

---
+++ 1.cpp
@@ -0,0 +1,35 @@
+typedef long long ll;
+#define debug(x) \
+    (std::cerr << #x << ": " << (x) << '\n')
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+#include <fstream>
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    std::vector<int> left(1000);
+    std::vector<int> right(1000);
+
+    // read input from 1.txt into left and right
+    std::ifstream infile("/home/reni/Documents/advent_2024/1.txt");
+    for (int i = 0; i < 1000 && infile; ++i) {
+        infile >> left[i] >> right[i];
+    }
+    infile.close();
+    
+    std::sort(left.begin(), left.end());
+    std::sort(right.begin(), right.end());
+
+    std::vector<int> diff(1000);
+    
+    int sum = std::inner_product(left.begin(), left.end(), right.begin(), 0, std::plus<>(),
+        [](int a, int b) { return std::abs(a - b); });
+
+    std::cout << "Sum: " << sum << '\n';
+
+    return 0;
+}
---
+++ 1_part2.cpp
@@ -0,0 +1,33 @@
+typedef long long ll;
+#define debug(x) \
+    (std::cerr << #x << ": " << (x) << '\n')
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+#include <fstream>
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    std::vector<int> left(1000);
+    std::vector<int> right(1000);
+
+    // read input from 1.txt into left and right
+    std::ifstream infile("/home/reni/Documents/advent_2024/1.txt");
+    for (int i = 0; i < 1000 && infile; ++i) {
+        infile >> left[i] >> right[i];
+    }
+    infile.close();
+    
+    int sum = 0;
+    for (int i = 0; i < 1000; ++i) {
+        int similarity_score = left[i] * std::count(right.begin(), right.end(), left[i]);
+        sum += similarity_score;
+    }
+
+    std::cout << "Sum: " << sum << '\n';
+
+    return 0;
+}
